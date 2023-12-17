b0652cb83992e4eb74871c78bcbd5a8560438e3b
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-12-17 16:24:10 +0330

    💄 add skelton style

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -4,9 +4,25 @@ function App() {
 
   return (
     <Layout>
-      <p className="px-1 py-10 bg-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
+      <main className="max-w-9xl mx-auto">
+        <div className="max-w-3xl mx-auto pt-16">
+          {/* card */}
+          <div className=" b-white border rounded-md bg-slate-200 h-64">
+
+          </div>
+          <button className="block mx-auto my-4"> 7 days ...</button>
+        </div>
+
+        {/* suggestion */}
+        <div className=" mx-auto w-full b-white border rounded-md bg-slate-200">
+          <div className="flex flex-row gap-1 lg:flex-wrap lg:justify-center lg:items-center lg:gap-3 overflow-x-auto">
+            {/* card */}
+            {[1, 2, 3, 4, 5, 6, 7].map((item) =>
+              <div className="bg-gray-600 w-80 h-80 min-w-[280px] lg:min-w-fit">{item}</div>)
+            }
+          </div>
+        </div>
+      </main>
     </Layout>
   )
 }
--- weather/tailwind.config.js
+++ weather/tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      maxWidth: {
+        '9xl': '1400px',
+      }
+    }
   },
   plugins: [],
 };
