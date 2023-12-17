6b28aa8bba4bc42164cb1542997fa13ecd4a78da
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-12-17 14:42:18 +0330

    🎉 being react project and add tailwind css

---
+++ weather/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+
+  return (
+    <>
+      <p className="px-1 bg-red-300">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
