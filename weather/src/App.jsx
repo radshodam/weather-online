527158380abdc823a93565e19447ea54a7f458e4
mohamadZareai 	☾ <mohamadzareai@gmail.com>
2023-12-17 14:48:26 +0330

    💄 add config tailwind for style

--- weather/src/App.jsx
+++ weather/src/App.jsx
@@ -2,7 +2,7 @@ function App() {
 
   return (
     <>
-      <p className="px-1 bg-red-300">
+      <p className="px-1 bg-red-500">
         Click on the Vite and React logos to learn more
       </p>
     </>
